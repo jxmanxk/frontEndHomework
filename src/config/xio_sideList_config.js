3eb6b0ee08beff2c2f413d1bd723bee8a5009795
jazz <312142485@qq.com>
2019-05-13 18:56:49 +0800

    'dev提交'

--- src/config/xio_sideList_config.js
+++ src/config/xio_sideList_config.js
@@ -2,13 +2,13 @@
  * @Author: 陈少雄_xio 
  * @Date: 2019-05-10 12:27:44 
  * @Last Modified by: 陈少雄_xio
- * @Last Modified time: 2019-05-10 19:20:28
+ * @Last Modified time: 2019-05-13 18:54:26
  */
 
 // xio_sideList 侧边栏组件的配置文件
     var sideList=[
         {
-            name:"测试模板1",
+            name:"测试模板1--xio",
             router: "/",//index_1st
             iconUrl:require("../images/ico-arrow_right.png"),
         },
